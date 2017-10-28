8df02cc409e9abacdf0b8301f7b57897e6829cc2
nbradf2 <nbradf2@gmail.com>
2017-10-28 08:38:14 -0500

    add option to delete recipes

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+// Retrieve the id of the item to be deleted from the request params, and then call ShoppingList.delete() with that value.  Send back a blank response with a 204 statis cpde/
 app.delete('/shopping-list/:id', (req, res) => {
   ShoppingList.delete(req.params.id);
   console.log(`Deleted shopping list item \`${req.params.id}\``);
@@ -76,6 +77,12 @@ app.post('/recipes', jsonParser, (req, res) => {
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+app.delete('/recipes/:id', (req, res) => {
+	Recipes.delete(req.params.id);
+	console.log(`Deleted recipe list item \`${req.params.id}\``);
+	res.status(204).end();
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
